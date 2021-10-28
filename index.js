f1f0278fc72238482648961384f3e0d02dd43536
sanjidaKabirSrity <sanjida.kabir.srity@gmail.com>
2021-10-28 07:03:02 +0600

    update

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ async function run(){
 run().catch(console.dir);
 
 app.get("/" , (req, res) => {
-   res.send("Running genious server");
+   res.send("Running genious car mechanic server");
 });
 
 app.listen(port , () => {
